54899297f7e2f51d5c0e293ae41190adda43f870
Christoph Herrmann <christoph.herrmann@7-tv.de>
2019-02-20 13:18:57 +0100

    Define all test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,15 +3,41 @@ const assert = require('power-assert')
 const sql = require('../')
 
 describe('sql', () => {
-  describe('Extract and bind values', () => {})
+  describe('Extract and bind values', () => {
+    it('should work with one value inside the query', () => {})
 
-  describe('Escape keys for tables and columns', () => {})
+    it('should work with one value at the end of the query', () => {})
 
-  describe('Extract and bind multiple values', () => {})
+    it('should work with multiple values', () => {})
+  })
 
-  describe('Extract and bind list of multiple values', () => {})
+  describe('Escape keys for tables and columns', () => {
+    it('should work with one key', () => {})
 
-  describe('Support pairs of column keys and values using as set of updates', () => {})
+    it('should work with multiple keys', () => {})
+  })
 
-  describe('Support pairs of column keys and values using as set of conditions', () => {})
+  describe('Extract and bind multiple values', () => {
+    it('should work with one value', () => {})
+
+    it('should work with multiple values', () => {})
+  })
+
+  describe('Extract and bind list of multiple values', () => {
+    it('should work with one multiple value', () => {})
+
+    it('should work with list of multiple values', () => {})
+  })
+
+  describe('Support pairs of column keys and values using as set of updates', () => {
+    it('should work with one pair', () => {})
+
+    it('should work with multiple pairs', () => {})
+  })
+
+  describe('Support pairs of column keys and values using as set of conditions', () => {
+    it('should work with one pair', () => {})
+
+    it('should work with multiple pairs', () => {})
+  })
 })
