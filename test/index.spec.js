0a91334835b733406f1c59bdd0e9f4a9647cbab4
Christoph Herrmann <christoph.herrmann@7-tv.de>
2019-02-20 13:01:29 +0100

    Use cases as describe to have multiple test cases in it

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,10 +3,15 @@ const assert = require('power-assert')
 const sql = require('../')
 
 describe('sql', () => {
-  it('should extract and bind values', () => {})
-  it('should escape keys for tables and columns', () => {})
-  it('should extract and bind multiple values', () => {})
-  it('should extract and bind list of multiple values', () => {})
-  it('should support pairs of column keys and values using as set of updates', () => {})
-  it('should support pairs of column keys and values using as set of conditions', () => {})
+  descripe('Extract and bind values', () => {})
+
+  descripe('Escape keys for tables and columns', () => {})
+
+  descripe('Extract and bind multiple values', () => {})
+
+  descripe('Extract and bind list of multiple values', () => {})
+
+  descripe('Support pairs of column keys and values using as set of updates', () => {})
+
+  descripe('Support pairs of column keys and values using as set of conditions', () => {})
 })
