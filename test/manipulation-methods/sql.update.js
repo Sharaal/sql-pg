bcb64974e222c7807b51289f30920d60d1fa4e19
Christoph Herrmann <developer@dragonprojects.de>
2019-07-03 16:58:17 +0200

    Some more tests for the schema support with tables

--- test/manipulation-methods/sql.update.js
+++ test/manipulation-methods/sql.update.js
@@ -38,6 +38,35 @@ describe('sql.update', () => {
     )
   })
 
+  it('update rows with the shorthand in a table with schema', async () => {
+    const expectedRowCount = 5
+    const client = {
+      query: sinon.fake.returns(Promise.resolve({ rowCount: expectedRowCount }))
+    }
+
+    sql.client = client
+    const actualRowCount = await sql.update(
+      ['schema', 'table'],
+      { column1: 'value1', column2: 'value2' },
+      { column3: 'value3', column4: 'value4' }
+    )
+
+    assert.equal(actualRowCount, expectedRowCount)
+
+    assert(client.query.calledOnce)
+
+    testSql(
+      client.query.getCall(0).args[0],
+      {
+        text: {
+          0: 'UPDATE "schema"."table" SET "column1" = $1, "column2" = $2 WHERE "column3" = $3 AND "column4" = $4',
+          5: 'UPDATE "schema"."table" SET "column1" = $6, "column2" = $7 WHERE "column3" = $8 AND "column4" = $9'
+        },
+        parameters: ['value1', 'value2', 'value3', 'value4']
+      }
+    )
+  })
+
   it('update rows with SQL Tag', async () => {
     const expectedRowCount = 5
     const client = {
