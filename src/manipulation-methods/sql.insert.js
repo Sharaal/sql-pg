a72f93fa412cc28542681a28bb98da50eee30659
Christoph Herrmann <christoph.herrmann@7-tv.de>
2019-10-28 13:17:02 +0100

    Refactor "sql.insert" to have one "sql.query" for both use cases

--- src/manipulation-methods/sql.insert.js
+++ src/manipulation-methods/sql.insert.js
@@ -2,24 +2,26 @@ module.exports = sql => {
   sql.defaultSerialColumn = sql.defaultSerialColumn || 'id'
 
   sql.insert = async (...params) => {
+    let array = true
     if (typeof params[0] === 'string' || Array.isArray(params[0])) {
       let [table, rows, { columns, serialColumn = sql.defaultSerialColumn } = {}] = params
-      let array = true
       if (!Array.isArray(rows)) {
-        rows = [rows]
         array = false
+        rows = [rows]
       }
       if (!columns) {
         columns = Object.keys(rows[0])
       }
-      const result = await sql.query(sql`INSERT INTO ${sql.table(table)} (${sql.columns(columns)}) VALUES ${sql.valuesList(rows, { columns })} RETURNING ${sql.column(serialColumn)}`)
-      if (!array) {
-        return result.rows[0][serialColumn]
-      }
-      return result.rows.map(row => row[serialColumn])
+      params = [
+        sql`INSERT INTO ${sql.table(table)} (${sql.columns(columns)}) VALUES ${sql.valuesList(rows, { columns })} RETURNING ${sql.column(serialColumn)}`,
+        { serialColumn }
+      ]
     }
     const [query, { serialColumn = sql.defaultSerialColumn } = {}] = params
     const result = await sql.query(query)
+    if (!array) {
+      return result.rows[0][serialColumn]
+    }
     return result.rows.map(row => row[serialColumn])
   }
 }
