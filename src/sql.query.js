4d3a1ceeb5a636407b0f2c4f54a4834d8162f5db
Christoph Herrmann <christoph.herrmann@7-tv.de>
2019-11-21 15:34:04 +0100

    Be consistent in throwing errors

--- src/sql.query.js
+++ src/sql.query.js
@@ -1,10 +1,10 @@
 module.exports = sql => {
   sql.query = (...params) => {
     if (typeof sql.client !== 'object' || typeof sql.client.query !== 'function') {
-      throw Error('Missing assignment of the initialized pg client to "sql.client"')
+      throw new Error('Missing assignment of the initialized pg client to "sql.client"')
     }
     if (typeof params[0] !== 'function' || params[0].symbol !== sql.symbol) {
-      throw Error('Only queries created with the sql tagged template literal are allowed')
+      throw new Error('Only queries created with the sql tagged template literal are allowed')
     }
     params[0] = params[0]()
     return sql.client.query(...params)
